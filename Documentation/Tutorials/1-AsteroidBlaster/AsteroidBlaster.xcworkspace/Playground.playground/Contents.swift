d6fe63b9716619ae512332e26b9e7f77250d593f
Hepburn, Magnus <mhepburn@lcs-mba3116.local>
2018-01-16 15:20:23 -0500

    got the falling rocks

--- Documentation/Tutorials/1-AsteroidBlaster/AsteroidBlaster.xcworkspace/Playground.playground/Contents.swift
+++ Documentation/Tutorials/1-AsteroidBlaster/AsteroidBlaster.xcworkspace/Playground.playground/Contents.swift
@@ -10,9 +10,52 @@ class AsteroidBlasterScene: Scene {
         let groundSize = Size(width: size.width, height: 100)
         let ground = Block(size: groundSize, textureCollectionName: "Ground" )
         add(ground)
-
         
+        
+        ground.position.x = center.x
+        ground.position.y = size.height - groundSize.height / 2
+        
+        let housesGroup = Group.name("Houses")
+        
+        for x in stride(from: center.x - 100, to: center.x + 150, by: 50) {
+            let house = Actor()
+            house.group = housesGroup
+            house.animation = Animation(name: "House", frameCount: 1, frameDuration: 0)
+            add(house)
+            
+            house.position.x = x
+            house.position.y = ground.rect.minY - house.size.height / 2
+            
+            timeline.repeat(withInterval: 2, using: self) { scene in
+                
+                let asteroid = Actor()
+                asteroid.animation = Animation(name: "Asteroid", frameCount: 1, frameDuration: 0)
+                scene.add(asteroid)
+            
+                let positionRange = scene.size.width - asteroid.size.width
+                let randomPosition = Metric(arc4random() % UInt32(positionRange))
+                asteroid.position.x = asteroid.size.width / 2 + randomPosition
+            
+                asteroid.velocity.dy = 400
+                
+            
+            }
+        }
+
+        timeline.repeat(withInterval: 2, using: self) { scene in
+            
+            let asteroid = Actor()
+            asteroid.animation = Animation(name: "Asteroid", frameCount: 1, frameDuration: 0)
+            scene.add(asteroid)
+            
+            let positionRange = scene.size.width - asteroid.size.width
+            let randomPosition = Metric(arc4random() % UInt32(positionRange))
+            asteroid.position.x = asteroid.size.width / 2 + randomPosition
+            
+            asteroid.velocity.dy = 400
+            
     }
+    
 }
 
 
@@ -20,17 +63,5 @@ let sceneSize = Size(width: 375, height: 667)
 let scene = AsteroidBlasterScene(size: sceneSize)
 PlaygroundPage.current.liveView = GameViewController(scene: scene)
 
-ground.position.x = center.x
-ground.position.y = size.height - groundSize.height / 2
 
-let housesGroup = Group.name("Houses")
-
-for x in stride(from: center.x - 100, to: center.x + 150, by: 50) {
-    let house = Actor()
-    house.group = housesGroup
-    house.animation = Animation(name: "House", frameCount: 1, frameDuration: 0)
-    add(house)
-    
-    house.position.x = x
-    house.position.y = ground.rect.minY - house.size.height / 2
 }
